cc4bb07f6ec57899a4d9c3dc4a4548be399a6d24
Maximilian Weigand <mweigand@mweigand.net>
2022-06-22 10:25:04 +0200

    [rockchip_ebc] add commented-out old position of lock

--- drivers/gpu/drm/rockchip/rockchip_ebc.c
+++ drivers/gpu/drm/rockchip/rockchip_ebc.c
@@ -1645,6 +1645,7 @@ static void rockchip_ebc_plane_atomic_update(struct drm_plane *plane,
 		return;
 	}
 
+	/* spin_lock(&ctx->queue_lock); */
 	list_splice_tail_init(&ebc_plane_state->areas, &ctx->queue);
 	spin_unlock(&ctx->queue_lock);
 
