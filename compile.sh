a649b319f32a4038770169da2af2ac4df05a1620
Maximilian Weigand <mweigand@mweigand.net>
2022-11-02 19:00:31 +0100

    still trying to build debian packages....

--- compile.sh
+++ compile.sh
@@ -6,6 +6,9 @@ if [ ! -d linux ]; then
 fi
 
 cd linux
+test -d pack && rm -r pack
+mkdir pack
+
 make clean
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- pinenote_defconfig
 make -j 2 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- all
@@ -13,18 +16,15 @@ make -j 2 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- all
 make -j 2 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- KBUILD_IMAGE=arch/arm64/boot/Image bindeb-pkg
 cd ..
 rm *dbg*.deb
-mv linux-image*.deb linux-image_with_uncompressed_image.deb
+# mv linux-image*.deb linux-image_with_uncompressed_image.deb
+rename -n --verbose 's/.deb/_no_compression.deb/' linux-image*
 cd linux
 
 make -j 2 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
 
-cd ..
 ls
-mv *.deb linux/pack
-cd ..
+mv ../*.deb pack/
 
-test -d pack && rm -r pack
-mkdir pack
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${PWD}/pack modules_install
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_PATH=${PWD}/pack dtbs_install
 cp ./arch/arm64/boot/dts/rockchip/rk3566-pinenote-v1.2.dtb pack/
