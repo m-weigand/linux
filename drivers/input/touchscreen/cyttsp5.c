e583ae11bb3102b08e96ebb9b078d1e7f25a64c9
Maximilian Weigand <mweigand@mweigand.net>
2023-03-16 14:35:50 +0100

    cyttsp5: implement proper (deep-)sleep and wakeup procedures for standby/resume when the device is not used as a wakeup device

--- drivers/input/touchscreen/cyttsp5.c
+++ drivers/input/touchscreen/cyttsp5.c
@@ -43,6 +43,7 @@
 #define HID_DESC_REG				0x1
 #define HID_INPUT_REG				0x3
 #define HID_OUTPUT_REG				0x4
+#define HID_COMMAND_REG             0x5
 
 #define REPORT_ID_TOUCH				0x1
 #define REPORT_ID_BTN				0x3
@@ -68,6 +69,7 @@
 #define HID_APP_OUTPUT_REPORT_ID		0x2F
 #define HID_BL_RESPONSE_REPORT_ID		0x30
 #define HID_BL_OUTPUT_REPORT_ID			0x40
+#define HID_RESPONSE_REPORT_ID      0xF0
 
 #define HID_OUTPUT_RESPONSE_REPORT_OFFSET	2
 #define HID_OUTPUT_RESPONSE_CMD_OFFSET		4
@@ -78,9 +80,15 @@
 #define HID_SYSINFO_BTN_MASK			GENMASK(7, 0)
 #define HID_SYSINFO_MAX_BTN			8
 
+#define HID_CMD_SET_POWER           0x8
+
+#define HID_POWER_ON                0x0
+#define HID_POWER_SLEEP             0x1
+
 #define CY_HID_OUTPUT_TIMEOUT_MS		200
 #define CY_HID_OUTPUT_GET_SYSINFO_TIMEOUT_MS	3000
 #define CY_HID_GET_HID_DESCRIPTOR_TIMEOUT_MS	4000
+#define CY_HID_SET_POWER_TIMEOUT		500
 
 /* maximum number of concurrent tracks */
 #define TOUCH_REPORT_SIZE			10
@@ -99,6 +107,14 @@
 #define TOUCH_REPORT_USAGE_PG_MAJ		0xFF010062
 #define TOUCH_REPORT_USAGE_PG_MIN		0xFF010063
 #define TOUCH_COL_USAGE_PG			0x000D0022
+
+#define SET_CMD_LOW(byte, bits) \
+    ((byte) = (((byte) & 0xF0) | ((bits) & 0x0F)))
+#define SET_CMD_HIGH(byte, bits)\
+    ((byte) = (((byte) & 0x0F) | ((bits) & 0xF0)))
+#define SET_CMD_OPCODE(byte, opcode) SET_CMD_LOW(byte, opcode)
+#define SET_CMD_REPORT_TYPE(byte, type) SET_CMD_HIGH(byte, ((type) << 4))
+#define SET_CMD_REPORT_ID(byte, id) SET_CMD_LOW(byte, id)
 
 /* System Information interface definitions */
 struct cyttsp5_sensing_conf_data_dev {
@@ -180,6 +196,7 @@ struct cyttsp5_hid_desc {
 struct cyttsp5 {
 	struct device *dev;
 	struct completion cmd_done;
+	struct completion cmd_command_done;
 	struct cyttsp5_sysinfo sysinfo;
 	struct cyttsp5_hid_desc hid_desc;
 	u8 cmd_buf[CYTTSP5_PREALLOCATED_CMD_BUFFER];
@@ -192,6 +209,7 @@ struct cyttsp5 {
 	struct regmap *regmap;
 	struct touchscreen_properties prop;
 	struct regulator *vdd;
+	bool is_wakeup_source;
 };
 
 /*
@@ -608,6 +626,86 @@ static int cyttsp5_hid_output_get_sysinfo(struct cyttsp5 *ts)
 	return cyttsp5_get_sysinfo_regs(ts);
 }
 
+static int cyttsp5_enter_sleep(struct cyttsp5 *ts)
+{
+	int rc;
+	u8 cmd[2];
+	u16 crc;
+
+	cmd[0] = 0;
+	cmd[1] = 0;
+
+	SET_CMD_REPORT_TYPE(cmd[0], 0);
+    SET_CMD_REPORT_ID(cmd[0], HID_POWER_SLEEP);
+    SET_CMD_OPCODE(cmd[1], HID_CMD_SET_POWER);
+
+	rc = cyttsp5_write(ts, HID_COMMAND_REG, cmd, 2);
+	if (rc) {
+		dev_err(ts->dev, "Failed to write command %d", rc);
+		return rc;
+	}
+
+	rc = wait_for_completion_interruptible_timeout(&ts->cmd_command_done,
+				msecs_to_jiffies(CY_HID_SET_POWER_TIMEOUT));
+	if (rc <= 0) {
+		dev_err(ts->dev, "HID output cmd execution timed out\n");
+		rc = -ETIMEDOUT;
+		return rc;
+	}
+
+	/* validate */
+	if ((ts->response_buf[2] != HID_RESPONSE_REPORT_ID)
+			|| ((ts->response_buf[3] & 0x3) != HID_POWER_SLEEP)
+			|| ((ts->response_buf[4] & 0xF) != HID_CMD_SET_POWER)){
+		rc = -EINVAL;
+		dev_err(ts->dev, "Validation of the sleep response failed\n");
+		return rc;
+	}
+
+	return 0;
+
+}
+
+static int cyttsp5_wakeup(struct cyttsp5 *ts)
+{
+	int rc;
+	u8 cmd[2];
+	u16 crc;
+
+	cmd[0] = 0;
+	cmd[1] = 0;
+
+	SET_CMD_REPORT_TYPE(cmd[0], 0);
+    SET_CMD_REPORT_ID(cmd[0], HID_POWER_ON);
+    SET_CMD_OPCODE(cmd[1], HID_CMD_SET_POWER);
+
+	rc = cyttsp5_write(ts, HID_COMMAND_REG, cmd, 2);
+	if (rc) {
+		dev_err(ts->dev, "Failed to write command %d", rc);
+		return rc;
+	}
+
+	rc = wait_for_completion_interruptible_timeout(&ts->cmd_command_done,
+				msecs_to_jiffies(CY_HID_SET_POWER_TIMEOUT));
+	if (rc <= 0) {
+		dev_err(ts->dev, "HID output cmd execution timed out\n");
+		rc = -ETIMEDOUT;
+		return rc;
+	}
+
+	/* validate */
+	if ((ts->response_buf[2] != HID_RESPONSE_REPORT_ID)
+			|| ((ts->response_buf[3] & 0x3) != HID_POWER_ON)
+			|| ((ts->response_buf[4] & 0xF) != HID_CMD_SET_POWER)){
+		rc = -EINVAL;
+		dev_err(ts->dev, "Validation of the sleep response failed\n");
+		return rc;
+	}
+
+	return 0;
+
+}
+
 static int cyttsp5_hid_output_bl_launch_app(struct cyttsp5 *ts)
 {
 	int rc;
@@ -725,6 +823,17 @@ static irqreturn_t cyttsp5_handle_irq(int irq, void *handle)
 		break;
 	case HID_BTN_REPORT_ID:
 		cyttsp5_btn_attention(ts->dev);
+		break;
+	case HID_RESPONSE_REPORT_ID:
+		/* printk(KERN_INFO "IRQ HID_RESPONSE_REPORT_ID: %i %i %i %i %i\n", */
+		/* 	ts->input_buf[0], */
+		/* 	ts->input_buf[1], */
+		/* 	ts->input_buf[2], */
+		/* 	ts->input_buf[3], */
+		/* 	ts->input_buf[4] */
+		/* ); */
+		memcpy(ts->response_buf, ts->input_buf, size);
+		complete(&ts->cmd_command_done);
 		break;
 	default:
 		/* It is not an input but a command response */
@@ -892,6 +1001,7 @@ static int cyttsp5_probe(struct device *dev, struct regmap *regmap, int irq,
 	dev_set_drvdata(dev, ts);
 
 	init_completion(&ts->cmd_done);
+	init_completion(&ts->cmd_command_done);
 
 	/* Power up the device */
 	ts->vdd = devm_regulator_get(dev, "vdd");
@@ -940,7 +1050,9 @@ static int cyttsp5_probe(struct device *dev, struct regmap *regmap, int irq,
 
 	if (device_property_read_bool(dev, "wakeup-source")) {
 		device_init_wakeup(dev, true);
-	}
+		ts->is_wakeup_source = true;
+	} else
+		ts->is_wakeup_source = false;
 
 	error = cyttsp5_startup(ts);
 	if (error) {
@@ -996,11 +1108,12 @@ MODULE_DEVICE_TABLE(i2c, cyttsp5_i2c_id);
 
 static int __maybe_unused cyttsp5_suspend(struct device *dev)
 {
-	/* struct cyttsp5 *ts = dev_get_drvdata(dev); */
-	/* struct i2c_client *client = to_i2c_client(dev); */
+	struct cyttsp5 *ts = dev_get_drvdata(dev);
 
-	// do we need to do something here? disable the irq?
-
+	if(!ts->is_wakeup_source){
+		cyttsp5_enter_sleep(ts);
+	}
+	/* cyttsp5_hid_output_enter_easywake(ts); */
 	return 0;
 }
 
@@ -1010,13 +1123,10 @@ static int __maybe_unused cyttsp5_resume(struct device *dev)
 	struct i2c_client *client = to_i2c_client(dev);
 	int error;
 
-	disable_irq(client->irq);
+	if(!ts->is_wakeup_source){
+		cyttsp5_wakeup(ts);
+	}
 
-	error = cyttsp5_clear_int(ts);
-	if (error) {
-		printk(KERN_ERR "cyttsp5 driver could not de-assert the interrupt line: err:%d\n", error);
-    }
-	enable_irq(client->irq);
 	return 0;
 }
 
