e1e6352851ad8a219d4a138b2a1e8d02ee682c7b
Maximilian Weigand <mweigand@mweigand.net>
2023-06-05 08:33:40 +0200

    [ci] still tuning the ci...

--- compile_v6-3.sh
+++ compile_v6-3.sh
@@ -24,19 +24,26 @@ rm *dbg*.deb
 # mv linux-image*.deb linux-image_with_uncompressed_image.deb
 rename 's/.deb/_no_compression.deb/' linux-image*
 
-# cd linux
+mv *.deb linux/pack/
+mv linux-update* linux/pack/
+
+cd linux
 # make -j 2 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LOCALVERSION=-pinenote-`date +%Y%m%d%H%M` KDEB_PKGVERSION="" bindeb-pkg
 # mv ../*.deb pack/
 
-# make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${PWD}/pack modules_install
-# make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_PATH=${PWD}/pack dtbs_install
-# cp ./arch/arm64/boot/dts/rockchip/rk3566-pinenote-v1.2.dtb pack/
-# cp ./arch/arm64/boot/Image pack/
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${PWD}/pack modules_install
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_PATH=${PWD}/pack dtbs_install
+cp ./arch/arm64/boot/dts/rockchip/rk3566-pinenote-v1.2.dtb pack/
+cp ./arch/arm64/boot/Image pack/
 # rename 's/.deb/_with_compression.deb/' linux-image*
-# cd pack
-# tar cvzf modules.tar.gz lib
-# rm -r lib
-# cd ../..
+cd pack
+tar cvzf modules.tar.gz lib
+rm -r lib
+cd ../..
+
+echo ""
+echo "LS PACK"
+ls pack
 
 # extract the results from the Docker container
 cp -r linux/pack /github/home/pack_v6.3
